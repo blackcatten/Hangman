8228605b240f3aec9b4a20c5b0000a5ee13fe115
blackcatten sweden <m_arnesson@hotmail.com>
2024-02-26 09:31:51 +0000

    add structure over hangman

--- run.py
+++ run.py
@@ -1,3 +1,51 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import os
+
+hangman = ["
+.-=-.      
+|   |,
+  |       
+  |       
+.-=-.      
+|   |,
+   _______
+  |       
+  |      
+.-=-.      
+|   |,
+   _______
+  |       |
+  |      
+.-=-.      
+|   |,
+   _______
+  |       |
+  |       O
+.-=-.     
+|   |,
+   _______
+  |       |
+  |      _O 
+.-=-.     
+|   |,
+   _______
+  |       |
+  |      _O_ 
+.-=-.     
+|   |,
+   _______
+  |       |
+  |      _O_ 
+.-=-.     | 
+|   |,
+   _______
+  |       |
+  |      _O_ 
+.-=-.     | 
+|   |    /,
+   _______
+  |       |
+  |      _O_ 
+.-=-.     | 
+|   |    /\ 
+"]
