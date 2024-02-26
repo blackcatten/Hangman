8c1571d00ac6856352db4da6215b5025d906dab2
blackcatten sweden <m_arnesson@hotmail.com>
2024-02-26 11:01:03 +0000

    add correct guesses and incorrect guesses

--- run.py
+++ run.py
@@ -1,55 +1,117 @@
 import random
 import os
 
-hangman = ["
+hangman = ['''
 .-=-.      
-|   |,
+|   |
+''',
+'''
   |       
   |       
 .-=-.      
-|   |,
+|   |
+''',
+'''
    _______
   |       
   |      
 .-=-.      
-|   |,
+|   |
+''',
+'''
    _______
   |       |
   |      
 .-=-.      
-|   |,
+|   |
+''',
+'''
    _______
   |       |
   |       O
 .-=-.     
-|   |,
+|   |
+''',
+'''
    _______
   |       |
   |      _O 
 .-=-.     
-|   |,
+|   |
+''',
+'''
    _______
   |       |
   |      _O_ 
 .-=-.     
-|   |,
+|   |
+''',
+'''
    _______
   |       |
   |      _O_ 
 .-=-.     | 
-|   |,
+|   |
+''',
+'''
    _______
   |       |
   |      _O_ 
-.-=-.     | 
-|   |    /,
+.-=-.   __| 
+|   |     
+''',
+'''
    _______
   |       |
   |      _O_ 
-.-=-.     | 
-|   |    /\ 
-"]
+.-=-.   __|__ 
+|   |    
+''']
 
 words = ['work', 'lion', 'mail', 'pork', 'play', 'game', 'frog', 'card', 'blue', 'fish']
 
 word = random.choice(words)
+
+correct_guesses = set()
+incorrect_guesses = set()
+
+turns = 10
+
+while turns > 0:
+ 
+    failed = 0
+ 
+    for letter in word:
+ 
+        if letter in correct_guesses:
+            print(letter, end=" ")
+ 
+        else:
+            print("_", end=" ")
+            failed += 1
+ 
+    if failed == 0:
+     
+        print("You Win")
+ 
+        print("The word is: ", word)
+        break
+
+    print()
+    guess = input("guess a letter:").lower()
+ 
+    if guess in correct_guesses or guess in incorrect_guesses:
+        print("You've already guessed that letter.")
+        continue
+    
+    if guess in word:
+        correct_guesses.add(guess)
+    else:
+        incorrect_guesses.add(guess)
+        turns -= 1
+        print(hangman[9 - turns])  
+        print("Wrong! You have", turns, "more guesses.")
+    
+    if turns == 0:
+        print("You Lose. The word was:", word)
+        break
